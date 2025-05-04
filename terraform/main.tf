f8eccbe3e763b21c32ebac803fd097404ab00964
nizwant <mateusznizwantowski@gmail.com>
2025-05-04 20:02:04 +0200

    feat: update terraform to create app engine

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,4 @@
-resource "google_compute_instance" "default" {
-  name         = var.vm_name
-  machine_type = "e2-micro"
-  zone         = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-12"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
-  }
-
-  metadata = {
-    ssh-keys = "test_username:${file("~/.ssh/id_rsa.pub")}"
-  }
+resource "google_app_engine_application" "app" {
+  project     = var.project_id
+  location_id = var.region
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,11 +2,8 @@ variable "project" {
   default = "cloud-computing-project-458205"
 }
 variable "region" {
-  default = "europe-north2"
+  default = "europe-central2"
 }
 variable "zone" {
   default = "europe-north2-a"
-}
-variable "vm_name" {
-  default = "my-test-vm"
 }
