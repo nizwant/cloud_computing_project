f4488ff1263d7cc0a3a59efb7a8dfb5b3ac9c2d0
nizwant <mateusznizwantowski@gmail.com>
2025-05-12 19:29:57 +0200

    feat: create cloud sql

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,42 +4,44 @@ resource "google_app_engine_application" "app" {
 }
 
 
-# resource "google_sql_database_instance" "default" {
-#   name             = "sql-db-instance"
-#   project          = var.project_id
-#   region           = var.region
-#   database_version = var.db_version
 
-#   settings {
-#     tier = var.db_tier
-#     ip_configuration {
-#       ipv4_enabled    = false
-#       private_network = google_compute_network.default.self_link
-#     }
-#   }
+# ------------ CLOUD-SQL -------------
+resource "google_sql_database_instance" "default" {
+  name             = "sql-db-instance"
+  project          = var.project_id
+  region           = var.region
+  database_version = var.db_version
 
-#   deletion_protection = false
-# }
+  settings {
+    tier = var.db_tier
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.default.self_link
+    }
+  }
+
+  deletion_protection = false
+}
 
 resource "google_compute_network" "default" {
   name = "project-vpc"
 }
 
-# resource "google_sql_database" "db" {
-#   name     = var.db_name
-#   instance = google_sql_database_instance.default.name
-# }
+resource "google_sql_database" "db" {
+  name     = var.db_name
+  instance = google_sql_database_instance.default.name
+}
 
 resource "random_password" "db_password" {
   length  = 16
   special = true
 }
 
-# resource "google_sql_user" "appuser" {
-#   name        = "appuser"
-#   instance    = google_sql_database_instance.default.name
-#   password_wo = random_password.db_password.result
-# }
+resource "google_sql_user" "appuser" {
+  name        = "appuser"
+  instance    = google_sql_database_instance.default.name
+  password_wo = random_password.db_password.result
+}
 
 data "google_app_engine_default_service_account" "default" {
   project = var.project_id
@@ -51,7 +53,7 @@ resource "google_project_iam_member" "appengine_sql_client" {
   member  = "serviceAccount:${data.google_app_engine_default_service_account.default.email}"
 }
 
-
+# -------- PUB-SUB -------------------
 resource "google_pubsub_topic" "my_topic" {
   name = "songs-to-process"
 }
@@ -62,6 +64,7 @@ resource "google_pubsub_subscription" "my_subscription" {
 }
 
 
+# -------- Artefact registry -------------------
 resource "google_artifact_registry_repository" "my_simple_repo" {
   location      = var.region
   repository_id = "test-repo-name"
