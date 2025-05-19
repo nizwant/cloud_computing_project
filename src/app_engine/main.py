5676cee12a9422f6a310cab145e37b3153e20528
nizwant <mateusznizwantowski@gmail.com>
2025-05-19 12:34:06 +0200

    feat: rename file to follow snake case

--- src/app_engine/main.py
+++ src/app_engine/main.py
@@ -15,14 +15,14 @@ def about():
 
 @app.route("/add_song")
 def add_song():
-    return render_template("add-song.html")
+    return render_template("add_song.html")
 
 
 @app.route("/list_songs")
 def list_songs():
-    return render_template("list-songs.html")
+    return render_template("list_songs.html")
 
 
-@app.route("/increment", methods=["POST"])
-def increment():
-    return redirect(url_for("index"))
+# @app.route("/increment", methods=["POST"])
+# def increment():
+#     return redirect(url_for("index"))
