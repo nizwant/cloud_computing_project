cc47866182d0f8d525cd81ebc8abecc9d16bb1fe
szuvarska <01171269@pw.edu.pl>
2025-06-06 18:11:32 +0200

    fix pre-commit

--- src/abracadabra/GCPFingerprintDB.py
+++ src/abracadabra/GCPFingerprintDB.py
@@ -46,7 +46,7 @@ class GCPFingerprintDB(AbstractFingerprintDB):
         song_id: int,
         download_dir: str = "../songs/temp",
         sr: int = 22050,
-        cookies: bool = False
+        cookies: bool = False,
     ):
         print(f"Downloading audio from {youtube_url}...")
 
