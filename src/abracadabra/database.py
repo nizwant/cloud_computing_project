7afd0821c633c7f24fa83bce0888ee50ae0995a9
szuvarska <01171269@pw.edu.pl>
2025-06-08 21:49:41 +0200

    fix showing match

--- src/abracadabra/database.py
+++ src/abracadabra/database.py
@@ -1,10 +1,17 @@
+import sys
+import os
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src"))
+)
+
 def create_fingerprint_db(db_type: str = "memory"):
     if db_type == "memory":
-        from src.abracadabra.InMemoryFingerprintDB import InMemoryFingerprintDB
+        from abracadabra.InMemoryFingerprintDB import InMemoryFingerprintDB
 
         return InMemoryFingerprintDB()
     elif db_type == "gcp":
-        from src.abracadabra.GCPFingerprintDB import GCPFingerprintDB
+        from abracadabra.GCPFingerprintDB import GCPFingerprintDB
 
         return GCPFingerprintDB()
     else:
